80c7e7d9e3144332656d53f90c00cc663d8d87a0
iampitchou <iampitchou91@gmail.com>
2024-01-12 16:08:40 +0100

    upgrade to patoche v2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
-
 this_is_a_map 'yes'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/patoche_cyber_list.ytyp'
 
-data_file 'DLC_ITYP_REQUEST' 'stream/patoche_cyber_list.ytyp'
+version '2.0.0'
+author 'Patoche#4702'
+
